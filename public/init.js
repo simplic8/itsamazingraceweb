86310de476c96dfd3038bbc77013265f5b366e65
simplic8 <simplic8@outlook.com>
2019-10-16 20:17:04 +0800

    v4

---
+++ public/init.js
@@ -0,0 +1,38 @@
+/*
+var initialPosition = {lat: 1.3339167, lng: 103.9654622};
+const cp1 = {lat: 1.3546627, lng: 103.9859627}; // T3_1
+const cp2 = {lat: 1.3576017, lng: 103.9875597}; // T3_2
+const cp3 = {lat: 1.3604657, lng: 103.9892967}; // J_1
+const cp4 = {lat: 1.3600317, lng: 103.9904447}; // J_2
+const cp5 = {lat: 1.3619089, lng: 103.9893716}; // T1_1
+const cp6 = {lat: 1.3610187, lng: 103.9911727}; // T1_2
+const cp7 = {lat: 1.3562326, lng: 103.9889757}; // T2_1
+const cp8 = {lat: 1.3540811, lng: 103.9880787}; // T2_2
+const cp9 = {lat: 1.3374532, lng: 103.9828052}; // T4_1
+const cp10 = {lat: 1.3393003, lng: 103.9835068}; // T4_2
+const cp11 = {lat: 1.4093858, lng: 103.8680694}; // B_1
+const cp12 = {lat: 1.4089909, lng: 103.8679227}; // B_2
+const sensiDist = 0.03;
+*/
+
+var initialPosition = {lat: 1.3339167, lng: 103.9654622};
+const cp1 = {lat: 1.3546627, lng: 103.9859627}; // T3_1
+//const cp2 = {lat: 1.3576017, lng: 103.9875597}; // T3_2
+const cp2 = {lat: 1.3600657, lng: 103.9896967}; // J_1
+//const cp4 = {lat: 1.3600317, lng: 103.9904447}; // J_2
+const cp3 = {lat: 1.3619089, lng: 103.9893716}; // T1_1
+//const cp6 = {lat: 1.3610187, lng: 103.9911727}; // T1_2
+const cp4 = {lat: 1.3562326, lng: 103.9889757}; // T2_1
+//const cp8 = {lat: 1.3540811, lng: 103.9880787}; // T2_2
+const cp5 = {lat: 1.3374532, lng: 103.9828052}; // T4_1
+//const cp10 = {lat: 1.3393003, lng: 103.9835068}; // T4_2
+const cp6 = {lat: 1.3352068, lng: 103.9620639}; // X_1
+//const cp12 = {lat: 1.4089909, lng: 103.8679227}; // B_2
+const sensiDist = 0.03;
+
+cp1Tasks = "<font style='color:#FF6633;font-style:italic'>You found Checkpoint 1 (T3_1)!</font><br> <font>Click on this <a href='https://drive.google.com/open?id=1iR_7xFnzB5sZkJdLg-GPCaC_1bsLy2pb' target='_new1' style='font-style:underline'>link</a> to get your tasks</font><br>";
+cp2Tasks = "<font style='color:#FF6633;font-style:italic'>You found Checkpoint 2 (J_1)!</font><br> <font>Click on this <a href='https://drive.google.com/open?id=1_lmmVEXlfT1yGd4lk4EqUDbw0TxTm-H1' target='_new2' style='font-style:underline'>link</a> to get your tasks</font><br>";
+cp3Tasks = "<font style='color:#FF6633;font-style:italic'>You found Checkpoint 3 (T1_1)!</font><br> <font>Click on this <a href='https://drive.google.com/open?id=1uds1AAbvFjPHSsKOrqC2acZr3Tq4sjhj' target='_new3' style='font-style:underline'>link</a> to get your tasks</font><br>";
+cp4Tasks = "<font style='color:#FF6633;font-style:italic'>You found Checkpoint 4 (T2_1)!</font><br> <font>Click on this <a href='https://drive.google.com/open?id=17Ki7cM9gy7ErxGTjeLW6qZ4lZ_1TSEIt' target='_new4' style='font-style:underline'>link</a> to get your tasks</font><br>";
+cp5Tasks = "<font style='color:#FF6633;font-style:italic'>You found Checkpoint 5 (T4_1)!</font><br> <font>Click on this <a href='https://drive.google.com/open?id=1UOE4v0k9YdUzsQh6XQaOso9bioF4YAZ-' target='_new5' style='font-style:underline'>link</a> to get your tasks</font><br>";
+cp6Tasks = "<font style='color:#FF6633;font-style:italic'>You found Checkpoint 6 (X_1)!</font><br> <font>Click on this <a href='https://drive.google.com/open?id=185zVl_scy0CTgULr21QvWxH5GXzsRMyc' target='_new6' style='font-style:underline'>link</a> to get your tasks</font><br>";
